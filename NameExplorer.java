7bccb16f7351edf8f1bbcf4955f6a32cf61e5353
Kent <1175379+kentcollins@users.noreply.github.com>
2025-02-02 06:30:29 +0000

    revised task descriptions.

--- NameExplorer.java
+++ NameExplorer.java
@@ -17,7 +17,7 @@ public class NameExplorer {
     /**
      * Task 2. Find the most popular name for a year for the indicated sex.
      *
-     * @param year    the year in question
+     * @param year the year in question
      * @param sex, "M" or "F"
      * @return the name with the most occurrences
      */
@@ -27,20 +27,32 @@ public class NameExplorer {
     }
 
     /**
-     * Task 3. Return a count of the number of babies born in a given year, by
-     * sex.
+     * Task 3. Look up the number of babies of the specified sex with the given name.
      *
-     * @param year
-     * @return the number of babies born with the indicated sex
+     * @param year the year of interest
+     * @param name the name to search for
+     * @param sex focus the lookup on babies of the specified sex
+     * @return the number of babies given the specified name.
      */
-    public static int numBabiesByGender(String year, String sex) {
+    public static int lookup(int year, int name, String sex) {
         // TODO implement this method
         return 0;
     }
 
     /**
-     * Task 4. Return a count of the total number of named babies born during a
-     * given year, regardless of sex.
+     * Task 4. Return a count of all babies born of a specific sex in a given year.
+     *
+     * @param year
+     * @return the number of babies born with the indicated sex
+     */
+    public static int numBabiesBySex(String year, String sex) {
+        // TODO implement this method
+        return 0;
+    }
+
+    /**
+     * Task 5. Return a count of the total number of named babies born during a
+     * given year.
      *
      * @param year
      * @return the number of named babies born
@@ -51,7 +63,7 @@ public class NameExplorer {
     }
 
     /**
-     * Task 5. Return a list holding the top ten names for the year, in decreasing
+     * Task 6. Return a list holding the top ten names for the year, in decreasing
      * popularity, no matter the sex.
      *
      * @param year
@@ -63,9 +75,7 @@ public class NameExplorer {
     }
 
     /**
-     * Task 6. Given a name, locate and return the NameEntry object from the name's
-     * most popular year (in terms of ranking). If a name has the same popularity
-     * across multiple years, return the NameEntry object from any one of them.
+     * Task 7.  Find the year in which the given name was most popular.
      *
      * @param name of interest
      * @return the NameEntry object of the year that name was most popular
@@ -76,7 +86,7 @@ public class NameExplorer {
     }
 
     /**
-     * Task 7. Often, several names might be considered nicknames for a common name.
+     * Task 8. Often, several names might be considered nicknames for a common name.
      * For example, "William", "Will", "Bill", "Billy", "Willie", and "Willy" are
      * all derived from the base name "William". Write a method that accepts an
      * array/list of names (strings) and returns the total count for all.
@@ -90,10 +100,9 @@ public class NameExplorer {
     }
 
     /**
-     * Task 8. What is the most popular name ever given that starts with the letter
-     * "X"?
+     * Task 9. Determine the most popular name ever given that starts with the specified letter.
      *
-     * @param startingLetter the year of interest
+     * @param startingLetter the letter of interest
      */
     public static String mostPopularStartingWith(String startingLetter) {
         // TODO implement this method
@@ -101,31 +110,25 @@ public class NameExplorer {
     }
 
     /**
-     * Additional challenges to consider:
-     * <p>
-     * When was the height of popularity for your own name?
-     * <p>
-     * How many different names were given to girls in 1880? in 1980?
-     * <p>
-     * How many names account for 50% of the babies named in 1880? in 1980?
-     * <p>
-     * What is the most popular initial letter?
-     * <p>
-     * Prepare an alphabetical list of names that have repeated letters, such as the
-     * "n" in "Donna", the "s" in "Massimo", or the "a" in "Pumbaa".
-     * <p>
-     * Find names that entered the top 10 in a dramatic or explosive way. For
-     * example, You might determine the percent change for each name that was NOT in
-     * the top 10 the previous year and from those, identify the name that entered
-     * the top 10 with the highest percentage change. Can you link this suddenly
-     * popularity to a movie, TV show, or historic event? "Only Murders in the Building --> Mabel"
+     * Task 10. Examine the list and replace names that were given to both boys and girls with a new, single
+     * name entry with the summed number of babies from each.  The original entries are removed from the list and
+     * the combined entry instance shows sex as "-" to indicate that the sex is no longer exclusively "M" or "F".
+     * @param names a list in which to replace names given to both sexes with a single entry
      */
+    public static void combineAcrossSex(ArrayList<NameEntry> names) {
+        // TODO implement this method
+    }
 
     public static void main(String[] args) {
         String mostPopularName1986 = mostPopularNameForYear("1986"); // Michael
         System.out.println("The most popular name in 1986 was: " + mostPopularName1986);
         String mostPopularName1986Female = mostPopularNameForYearBySex("1986", "F"); // Jessica
         System.out.println("Most popular female name in 1986: " + mostPopularName1986Female);
-        // etc.
+
+        // In addition to implementing and testing the task methods above, research and answer the following:
+        // Q1 - do boys or girls have more diverse naming?
+        // Q2 - is there a difference in average name length between boys and girls?
+        // Q3 - what have proven to be the most popular names over time?
+
     }
 }
